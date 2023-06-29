ca4512be42db100faf4d61d74ac7c86bf092615a
Rashford <mikearowo@gmail.com>
2023-06-29 09:41:34 +0100

    new

---
+++ hardhat/contracts/CryptoDev.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.16;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Whitelist.sol";
+
+contract CryptoDevs is ERC721Enumerable, Ownable {
+ //  _price is the price of one Crypto Dev NFT
+ uint256 constant public _price = 0.01 ether;
+
+ // Max number of CryptoDevs that can ever exist
+ uint256 constant public
+}
--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -2,3 +2,18 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env" });
 
 const QUICKNODE_HTTP_URL = process.env.RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    goerli: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
