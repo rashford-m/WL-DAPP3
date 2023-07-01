c551a538ee3f43b5cfe47fe0940d5d7e2a5dbf9e
Rashford <mikearowo@gmail.com>
2023-07-01 14:30:40 +0100

    new

--- hardhat/contracts/CryptoDev.sol
+++ hardhat/contracts/CryptoDev.sol
@@ -7,16 +7,28 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Whitelist.sol";
 
 contract CryptoDevs is ERC721Enumerable, Ownable {
- //  _price is the price of one Crypto Dev NFT
- uint256 constant public _price = 0.01 ether;
+    //  _price is the price of one Crypto Dev NFT
+    uint256 public constant _price = 0.01 ether;
 
- // Max number of CryptoDevs that can ever exist
- uint256 constant public maxTokenIds = 20;
+    // Max number of CryptoDevs that can ever exist
+    uint256 public constant maxTokenIds = 20;
 
- // Whitelist contract instance
- Whitelist whitelist;
+    // Whitelist contract instance
+    Whitelist whitelist;
 
-  // Number of tokens reserved for whitelisted members
-  uint256 public reservedTokens;
-  uint256 public reservedTokensClaimed = 0;
+    // Number of tokens reserved for whitelisted members
+    uint256 public reservedTokens;
+    uint256 public reservedTokensClaimed = 0;
+
+    /**
+     * @dev ERC721 constructor takes in a `name` and a `symbol` to the token collection.
+     * name in our case is `Crypto Devs` and symbol is `CD`.
+     * Constructor for Crypto Devs takes in the baseURI to set _baseTokenURI for the collection.
+     * It also initializes an instance of whitelist interface.
+     */
+
+    constructor(address whitelistContract) ERC721("Crypto Devs", "CD") {
+        whitelist = Whitelist(whitelistContract);
+        reservedTokens = whitelist.maxWhitelistedAddresses();
+    }
 }
