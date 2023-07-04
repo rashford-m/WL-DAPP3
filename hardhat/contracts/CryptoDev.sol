ffaea16e5fac3099f5fba8e7bed1a7e36a635a2f
Rashford <mikearowo@gmail.com>
2023-07-04 16:59:32 +0100

    new

--- hardhat/contracts/CryptoDev.sol
+++ hardhat/contracts/CryptoDev.sol
@@ -31,7 +31,37 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
         whitelist = Whitelist(whitelistContract);
         reservedTokens = whitelist.maxWhitelistedAddresses();
     }
+
     function mint() public payable {
-        
+        // Make sure we always leave enough room for whitelist reservations
+        require(
+            totalSupply() + reservedTokens - reservedTokensClaimed <
+                maxTokenIds,
+            "EXCEEDED_MAX_SUPPLY"
+        );
+
+        // If user is part of the whitelist, make sure there is still reserved tokens left
+        if (whitelist.whitelistedAddresses(msg.sender) && msg.value < _price) {
+            // Make sure user doesn't already own an NFT
+            require(balanceOf(msg.sender) == 0, "ALREADY_OWNED");
+            reservedTokensClaimed += 1;
+        } else {
+            // If user is not part of the whitelist, make sure they have sent enough ETH
+            require(msg.value >= _price, "NOT_ENOUGH_ETHER");
+        }
+        uint256 tokenId = totalSupply();
+        _safeMint(msg.sender, tokenId);
+    }
+
+    /**
+     * @dev withdraw sends all the ether in the contract
+     * to the owner of the contract
+     */
+    function withdraw() public onlyOwner {
+        address _owner = owner();
+        uint256 amount = address(this).balance;
+        (bool sent, ) = _owner.call{value: amount}("");
+        require(sent, "Failed to send Ether");
     }
 }
+
