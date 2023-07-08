1a66fd542cef08823bc99a4ad9dbb831040374cd
Rashford <mikearowo@gmail.com>
2023-07-08 14:47:08 +0100

    new

--- hardhat/scripts/deploy-nft.js
+++ hardhat/scripts/deploy-nft.js
@@ -3,3 +3,7 @@ const hre = require("hardhat");
 async function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
+async function main() {
+     // Deploy the CryptoDevs Contract
+     
+}
