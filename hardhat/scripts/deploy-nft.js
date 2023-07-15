e78503a4ef2484769b39cce70db81493d0267f72
Rashford <mikearowo@gmail.com>
2023-07-15 15:54:36 +0100

    new

--- hardhat/scripts/deploy-nft.js
+++ hardhat/scripts/deploy-nft.js
@@ -1,9 +1,34 @@
 const hre = require("hardhat");
 
 async function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 async function main() {
-     // Deploy the CryptoDevs Contract
-     
+  // Deploy the CryptoDevs Contract
+  const nftContract = await hre.ethers.deployContract("CryptoDevs", [
+    "0x1810e51e7cfe6b4c174ccfe56b6c9c8002b50305",
+  ]);
+
+  // wait for the contract to deploy
+  await nftContract.waitForDeployment();
+
+  // print the address of the deployed contract
+  console.log("NFT Contract Address:", nftContract.target);
+
+  // Sleep for 30 seconds while Etherscan indexes the new contract deployment
+  await sleep(30 * 1000); // 30s = 30 * 1000 milliseconds
+
+  // Verify the contract on etherscan
+  await hre.run("verify:verify", {
+    address: nftContract.target,
+    constructorArguments: ["0x1810e51e7cfe6b4c174ccfe56b6c9c8002b50305"],
+  });
 }
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
